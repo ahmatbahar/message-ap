b843d1fab7a0d97a3e1e29523b0ff545d3ba3b02
ahmat bahar <ahmatmahamatbahar@gmail.com>
2021-01-04 22:16:55 +0100

    change versin of ruby

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,7 @@ module MChat
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-    group :productiondo
+    # the framework and any gems in your application
       gem 'pg'
-    end
   end
 end
